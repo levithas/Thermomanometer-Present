85f34b23d85faa62a0822a2ba89f4d217927101d
levithas <levithas_tiamara@web.de>
2024-08-25 01:06:48 +0200

    Refactor:\n- Small linting in message_state.cpp\n- Refaktoring of functions into morser.cpp file

--- Thermomanometer/message_state.cpp
+++ Thermomanometer/message_state.cpp
@@ -3,21 +3,21 @@
 #include "press_temp_state.h"
 
 uint32_t MessageState::handle(Context& context) 
-    {
-      // HEX Message Display
-      context.getDV().showHexMessage(message, messagePosition);
-      messagePosition++;
-    
-      if(messagePosition >= message.size())
-      {
-        if(messageRepeats >= MaxMessageRepeats)
-        {
-          context.setState(new PressTempState());
-          return 0;
-        }
-        messageRepeats++;
-        messagePosition = 0;
-      }
+{
+  // HEX Message Display
+  context.getDV().showHexMessage(message, messagePosition);
+  messagePosition++;
 
-      return 4000;
+  if(messagePosition >= message.size())
+  {
+    if(messageRepeats >= MaxMessageRepeats)
+    {
+      context.setState(new PressTempState());
+      return 0;
     }
+    messageRepeats++;
+    messagePosition = 0;
+  }
+
+  return 4000;
+}
--- Thermomanometer/morser.h
+++ Thermomanometer/morser.h
@@ -1,6 +1,8 @@
 #ifndef MORSER_H
 #define MORSER_H
 
+#include <Arduino.h>
+#include <cstdint>
 #include <map>
 
 #define LED_PIN 15
@@ -9,83 +11,38 @@ enum CODE {
   DIT, LONG, SHORT, NEXT, WORD, PAUSE
 };
 
-
-
 class Morser {
-private:
-  std::map<CODE, uint8_t> code
-  {
-    {CODE::DIT, 3},
-    {CODE::LONG, 9},
-    {CODE::SHORT, 3},
-    {CODE::NEXT, 9},
-    {CODE::WORD, 21},
-    {CODE::PAUSE, 60}
-  };
-
-  const static uint8_t codeLength = 55;
-  CODE message[codeLength] = {
-    LONG, LONG, NEXT, SHORT, LONG, NEXT, LONG, SHORT, LONG, SHORT, NEXT, SHORT, SHORT, SHORT, SHORT
-  , WORD, LONG , LONG, NEXT, SHORT, SHORT, NEXT, LONG, SHORT, LONG, SHORT, NEXT, SHORT, SHORT, SHORT, SHORT
-  , WORD, SHORT, NEXT, SHORT, SHORT, NEXT, SHORT, SHORT, SHORT, NEXT, LONG, SHORT, LONG, NEXT, SHORT, LONG, NEXT, SHORT, LONG, SHORT, SHORT, NEXT, LONG, PAUSE
-  };
+  private:
+    std::map<CODE, uint8_t> codeLengthMap = {
+      {CODE::DIT, 3},
+      {CODE::LONG, 9},
+      {CODE::SHORT, 3},
+      {CODE::NEXT, 9},
+      {CODE::WORD, 21},
+      {CODE::PAUSE, 60}
+    };
   
-  uint8_t currentLetter = 0;
-  uint8_t step = 0;
-  bool pause = false;
-  uint8_t pausedits = 0;
-
-  const uint8_t MAX_REPEATS = 5;
-  uint8_t repeats = 0;
+    const static uint8_t codeLength = 55;
+    CODE message[codeLength] = {
+      LONG, LONG, NEXT, SHORT, LONG, NEXT, LONG, SHORT, LONG, SHORT, NEXT, SHORT, SHORT, SHORT, SHORT
+    , WORD, LONG , LONG, NEXT, SHORT, SHORT, NEXT, LONG, SHORT, LONG, SHORT, NEXT, SHORT, SHORT, SHORT, SHORT
+    , WORD, SHORT, NEXT, SHORT, SHORT, NEXT, SHORT, SHORT, SHORT, NEXT, LONG, SHORT, LONG, NEXT, SHORT, LONG, NEXT, SHORT, LONG, SHORT, SHORT, NEXT, LONG, PAUSE
+    };
+    
+    uint8_t currentLetter = 0;
+    uint8_t step = 0;
+    bool pause = false;
+    uint8_t pausedits = 0;
+  
+    const uint8_t MAX_REPEATS = 5;
+    uint8_t repeats = 0;
 
   public:
     Morser() {
       pinMode(LED_PIN, OUTPUT);
-    }
+    };
 
-    void update() {
-      if(repeats >= MAX_REPEATS)
-        return;
-      
-      if(pause) {
-        digitalWrite(LED_PIN, LOW);
-        pausedits++;
-        if(pausedits >= code[CODE::DIT])
-        {
-          pause = false;
-        }
-        return;
-      }
-
-      switch(message[currentLetter])
-      {
-        case LONG:
-        case SHORT:
-          digitalWrite(LED_PIN, HIGH);
-          break;
-        case NEXT:
-        case WORD:
-        case PAUSE:
-          digitalWrite(LED_PIN, LOW);
-          break;
-        default:
-          break;
-      }
-
-      step++;
-      if(step >= code[message[currentLetter]])
-      {
-        currentLetter++;
-        pause = true;
-        step = 0;
-      }
-
-      if(currentLetter >= codeLength)
-      {
-        currentLetter = 0;
-        repeats++;
-      }
-    }
+    void update();
 };
 
 #endif
